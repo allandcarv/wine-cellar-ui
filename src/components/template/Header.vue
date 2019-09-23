c68732dd21fff0d5002c6ab5dc99e3c6c21a0b72
Allan Carvalho <allandcarv@gmail.com>
2019-09-23 12:07:31 -0300

    Header Component

--- src/components/template/Header.vue
+++ src/components/template/Header.vue
@@ -1,7 +1,10 @@
 <template>
   <header class="header">
-    <h1>My Wine Cellar</h1>
-    <span>Hello Visitor!!</span>
+    <h1 class="header__title">My Wine Cellar</h1>
+    <div class="header__user">
+      <i class="fa fa-user-circle" />
+      <span>Hello Visitor!!</span>
+    </div>
   </header>
 </template>
 
@@ -15,9 +18,30 @@ export default {
 .header {
   grid-area: header;
   background-color: #7159c1;
+  padding: 0 60px;
 
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+
+  color: #fff;
+}
+
+.header .header__user {
+  height: 100%;
+  padding: 0 20px;
+
+  display: flex;
+  align-items: center;
+
+  font-size: 1.4rem;
+}
+
+.header .header__user:hover {
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+.header__user i {
+  margin-right: 10px;
 }
 </style>
